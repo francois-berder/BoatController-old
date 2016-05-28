8085a04d0c949adc0cdc79f49a1091565d7c9e2b
Francois Berder <fberder@outlook.fr>
2016-05-28 15:44:54 +0100

    import initial code
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ mcc_generated_files/uart1.c
@@ -0,0 +1,136 @@
+/**
+  UART1 Generated Driver File
+
+  @Company
+    Microchip Technology Inc.
+
+  @File Name
+    uart1.c
+
+  @Summary
+    This is the generated driver implementation file for the UART1 driver using MPLAB(c) Code Configurator
+
+  @Description
+    This header file provides implementations for driver APIs for UART1.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - v3.00
+        Device            :  PIC24FJ128GB202
+        Driver Version    :  2.00
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 1.26
+        MPLAB             :  MPLAB X 3.20
+*/
+
+/*
+Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ */
+
+/**
+  Section: Included Files
+*/
+#include "uart1.h"
+
+/**
+  Section: UART1 APIs
+*/
+
+void UART1_Initialize(void)
+{
+    // Set the UART1 module to the options selected in the user interface.
+
+    // STSEL 1; IREN disabled; PDSEL 8N; UARTEN enabled; RTSMD disabled; USIDL disabled; WAKE disabled; ABAUD disabled; LPBACK disabled; BRGH disabled; URXINV disabled; UEN TX_RX; 
+    U1MODE = 0x8000;
+
+    // OERR NO_ERROR_cleared; URXISEL RX_ONE_CHAR; UTXBRK COMPLETED; UTXEN disabled; ADDEN disabled; UTXISEL0 TX_ONE_CHAR; UTXINV disabled; 
+    U1STA = 0x0000;
+
+    // BaudRate = 38400; Frequency = 16000000 Hz; U1BRG 25; 
+    U1BRG = 0x0019;
+
+    // ADMADDR 0; ADMMASK 0; 
+    U1ADMD = 0x0000;
+
+    // T0PD 1 ETU; PTRCL T0; TXRPT Retransmits the error byte once; CONV Direct; SCEN disabled; 
+    U1SCCON = 0x0000;
+
+    // TXRPTIF disabled; TXRPTIE disabled; WTCIF disabled; WTCIE disabled; PARIE disabled; GTCIF disabled; GTCIE disabled; RXRPTIE disabled; RXRPTIF disabled; 
+    U1SCINT = 0x0000;
+
+    // GTC 0; 
+    U1GTC = 0x0000;
+
+    // WTCL 0; 
+    U1WTCL = 0x0000;
+
+    // WTCH 0; 
+    U1WTCH = 0x0000;
+
+    U1STAbits.UTXEN = 1;
+}
+
+
+uint8_t UART1_Read(void)
+{
+    while(!(U1STAbits.URXDA == 1))
+    {
+        
+    }
+
+    if ((U1STAbits.OERR == 1))
+    {
+        U1STAbits.OERR = 0;
+    }
+
+    
+
+    return U1RXREG;
+}
+
+void UART1_Write(uint8_t txData)
+{
+    while(U1STAbits.UTXBF == 1)
+    {
+        
+    }
+
+    U1TXREG = txData;    // Write the data byte to the USART.
+}
+
+UART1_STATUS UART1_StatusGet (void)
+{
+    return U1STA;
+}
+
+int __attribute__((__section__(".libc.write"))) write(int handle, void *buffer, unsigned int len) {
+    int i;
+    while(U1STAbits.TRMT == 0);  
+    for (i = len; i; --i)
+    {
+        while(U1STAbits.TRMT == 0);
+        U1TXREG = *(char*)buffer++;        
+    }
+    return(len);
+}
+
+/**
+  End of File
+*/
---
+++ mcc_generated_files/uart1.h
@@ -0,0 +1,227 @@
+/**
+  UART1 Generated Driver API Header File
+
+  @Company
+    Microchip Technology Inc.
+
+  @File Name
+    uart1.h
+
+  @Summary
+    This is the generated header file for the UART1 driver using MPLAB(c) Code Configurator
+
+  @Description
+    This header file provides APIs for driver for UART1.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - v3.00
+        Device            :  PIC24FJ128GB202
+        Driver Version    :  2.00
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 1.26
+        MPLAB             :  MPLAB X 3.20
+*/
+
+/*
+Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ */
+
+#ifndef _UART1_H
+#define _UART1_H
+
+/**
+  Section: Included Files
+*/
+
+#include <xc.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    extern "C" {
+
+#endif
+
+/**
+  Section: Data Types
+*/
+
+/** UART1 Driver Hardware Flags
+
+  @Summary
+    Specifies the status of the hardware receive or transmit
+
+  @Description
+    This type specifies the status of the hardware receive or transmit.
+    More than one of these values may be OR'd together to create a complete
+    status value.  To test a value of this type, the bit of interest must be
+    AND'ed with value and checked to see if the result is non-zero.
+*/
+typedef enum
+{
+    /* Indicates that Receive buffer has data, at least one more character can be read */
+    UART1_RX_DATA_AVAILABLE
+        /*DOM-IGNORE-BEGIN*/  = (1 << 0) /*DOM-IGNORE-END*/,
+    
+    /* Indicates that Receive buffer has overflowed */
+    UART1_RX_OVERRUN_ERROR
+        /*DOM-IGNORE-BEGIN*/  = (1 << 1) /*DOM-IGNORE-END*/,
+
+    /* Indicates that Framing error has been detected for the current character */
+    UART1_FRAMING_ERROR
+        /*DOM-IGNORE-BEGIN*/  = (1 << 2) /*DOM-IGNORE-END*/,
+
+    /* Indicates that Parity error has been detected for the current character */
+    UART1_PARITY_ERROR
+        /*DOM-IGNORE-BEGIN*/  = (1 << 3) /*DOM-IGNORE-END*/,
+
+    /* Indicates that Receiver is Idle */
+    UART1_RECEIVER_IDLE
+        /*DOM-IGNORE-BEGIN*/  = (1 << 4) /*DOM-IGNORE-END*/,
+
+    /* Indicates that the last transmission has completed */
+    UART1_TX_COMPLETE
+        /*DOM-IGNORE-BEGIN*/  = (1 << 8) /*DOM-IGNORE-END*/,
+
+    /* Indicates that Transmit buffer is full */
+    UART1_TX_FULL
+        /*DOM-IGNORE-BEGIN*/  = (1 << 9) /*DOM-IGNORE-END*/
+
+}UART1_STATUS;
+
+/**
+  Section: Macro Declarations
+*/
+
+#define UART1_DataReady  (U1STAbits.URXDA == 1)
+
+/**
+  Section: UART1 APIs
+*/
+
+/**
+  @Summary
+    Initialization routine that takes inputs from the UART1 GUI.
+
+  @Description
+    This routine initializes the UART1 driver.
+    This routine must be called before any other UART1 routine is called.
+
+  @Preconditions
+    None
+
+  @Param
+    None
+
+  @Returns
+    None
+
+  @Comment
+    
+*/
+void UART1_Initialize(void);
+
+/**
+  @Summary
+    Read a byte of data from the UART1.
+
+  @Description
+    This routine reads a byte of data from the UART1.
+
+  @Preconditions
+    UART1_Initialize() function should have been called
+    before calling this function. This is a blocking function.
+    So the recieve status should be checked to see
+    if the receiver is not empty before calling this function.
+
+  @Param
+    None
+
+  @Returns
+    A data byte received by the driver.
+*/
+uint8_t UART1_Read(void);
+
+ /**
+  @Summary
+    Writes a byte of data to the UART1.
+
+  @Description
+    This routine writes a byte of data to the UART1.
+
+  @Preconditions
+    UART1_Initialize() function should have been called
+    before calling this function. The transfer status should be checked to see
+    if transmitter is not busy before calling this function.
+
+  @Param
+    txData  - Data byte to write to the UART1
+
+  @Returns
+    None
+*/
+void UART1_Write(uint8_t txData);
+
+/**
+  @Summary
+    Returns the transmitter and receiver status
+
+  @Description
+    This returns the transmitter and receiver status. The returned status may 
+    contain a value with more than one of the bits
+    specified in the UART1_STATUS enumeration set.  
+    The caller should perform an "AND" with the bit of interest and verify if the
+    result is non-zero (as shown in the example) to verify the desired status
+    bit.
+
+  @Preconditions
+    UART1_Initializer function should have been called 
+    before calling this function
+
+  @Param
+    None.
+
+  @Returns
+    A UART1_STATUS value describing the current status 
+    of the transfer.
+
+  @Example
+    <code>
+        while(!(UART1_StatusGet & UART1_TX_COMPLETE ))
+        {
+           // Wait for the tranmission to complete
+        }
+    </code>
+*/
+
+UART1_STATUS UART1_StatusGet (void );
+
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    }
+
+#endif
+
+#endif  // _UART1_H
+/**
+ End of File
+*/

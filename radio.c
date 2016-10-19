86d3940c2a0b07e12988b840bbd2dc01eddfb964
Francois Berder <fberder@outlook.fr>
2016-10-19 07:57:09 +0100

    radio: add documentation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- radio.c
+++ radio.c
@@ -1,3 +1,31 @@
+/**
+ * Radio module of the Boat Controller.
+ *
+ * The radio module is in charge of converting signals from the
+ * radio receiver to numbers. It computes the length of the pulse
+ * on two channels.
+ *
+ * The pulse length is expected to be in range 1-2ms (neutral: 1.5ms).
+ * Using Interrupt on Change to detect edges and by recording the
+ * value of Timer 5 at both edges. It is possible to infer the length
+ * of the pulse.
+ * The Timer 5 is configured such that:
+ *
+ *      Joystick: Left                Neutral              Right
+ *  Pulse Length: 1 ms  -------------- 1.5ms -------------- 2 ms
+ *  Timer5 value: 2000                  3000                4000
+ *
+ * It has been found that the radio receiver skips pulses on channel 0.
+ * Also, it happens that the falling edge is sometimes not detected. In
+ * this case, around 50000 (equal to 25ms) is removed from the result.
+ *
+ * The radio receiver sends a pulse on each channel every 25ms. Considering
+ * that the radio overwrites existing data if the buffer is full, it is
+ * expected that the user will read fast enough from the buffer. Hence,
+ * it has up to (BUFFER_SIZE * 25) ms to read the data.
+ */
+
+
 #include <xc.h>
 #include <string.h>
 #include "mcc_generated_files/ic1.h"
@@ -5,12 +33,12 @@
 #include "log.h"
 #include "radio.h"
 
-#define BUFFER_SIZE     (16)
+#define BUFFER_SIZE     (16)		/* Must be a power of 2 */
 
 static uint16_t start[CHANNEL_CNT] = { 0, 0 };
 static bool pin_high[CHANNEL_CNT] = { false, false };
 
-static struct 
+static struct
 {
     uint16_t entries[BUFFER_SIZE];
     uint8_t entries_cnt;
@@ -42,6 +70,12 @@ static void RADIO_disable_interrupts(const uint8_t channel)
         LOG_ERR("radio: cannot disable interrupts with invalid channel.");
 }
 
+/**
+ * @brief Returns value recorded by IoC module using Timer 5.
+ *
+ * @param[in] channel
+ * @return 16bit value from Timer 5
+ */
 static uint16_t RADIO_read_ic(const uint8_t channel)
 {
     if (channel == DIRECTION_CHANNEL) {
@@ -55,6 +89,14 @@ static uint16_t RADIO_read_ic(const uint8_t channel)
     }
 }
 
+/**
+ * @brief Add an entry to the buffer.
+ *
+ * Notice that if the buffer is full, it overwrites existing data.
+ *
+ * @param[in] channel
+ * @param[in] data
+ */
 static void RADIO_add_entry(const uint8_t channel, const uint16_t data)
 {
     uint8_t index = (buffer[channel].head + buffer[channel].entries_cnt) % BUFFER_SIZE;
@@ -75,7 +117,7 @@ void RADIO_update(const uint8_t channel)
         while (!IC1_IsCaptureBufferEmpty())
             start[channel] = RADIO_read_ic(channel);
         pin_high[channel] = true;
-        
+
         /* interrupt on falling edge */
         if(channel == DIRECTION_CHANNEL)
             IC1CON1bits.ICM = 0x2;
@@ -86,13 +128,13 @@ void RADIO_update(const uint8_t channel)
         while (!IC1_IsCaptureBufferEmpty())
             end = RADIO_read_ic(channel);
         entry = end - start[channel];
-        
+
         /* It can happen that we skip one edge and take an extra 25ms
-         * These 25ms are roughly equal to 50150. 
+         * These 25ms are roughly equal to 50150.
          */
         if (entry > 45000)
             entry -= 50150;
-        LOG_DBG("entry%u= %u, start= %u, end= %u\n", channel, entry, start[channel], end);
+        //LOG_DBG("entry%u= %u, start= %u, end= %u\n", channel, entry, start[channel], end);
         pin_high[channel] = false;
 
         /* interrupt on rising edge */
@@ -114,20 +156,23 @@ uint16_t RADIO_buffer_read(const uint8_t channel)
 {
     uint16_t data = 0;
     uint8_t index = 0;
-    
+
     //RADIO_disable_interrupts(channel);
-    
+
     if (RADIO_is_buffer_empty(channel)) {
         LOG_WARN("radio: read from empty buffer on channel %d", channel);
         //RADIO_enable_interrupts(channel);
         return 0;
     }
     index = buffer[channel].head;
+
+	/* head = (head + 1) % BUFFER_SIZE */
     buffer[channel].head = (buffer[channel].head + 1) & (BUFFER_SIZE - 1);
+
     --buffer[channel].entries_cnt;
     data = buffer[channel].entries[index];
-    
+
     //RADIO_enable_interrupts(channel);
-    
+
     return data;
 }
--- radio.h
+++ radio.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   radio.h
  * Author: francois
  *
@@ -14,14 +14,47 @@ extern "C" {
 
 #include <stdbool.h>
 #include <stdint.h>
-    
+
 #define DIRECTION_CHANNEL       (0)
 #define SPEED_CHANNEL           (1)
 #define CHANNEL_CNT             (2)
-    
+
+/**
+ * @brief Initialise variables to zero.
+ *
+ * Note that this function must be called before SYSTEM_Initialize()
+ */
 void RADIO_init(void);
+
+/**
+ * @brief Process an edge.
+ *
+ * This function should always be called from interrupt handler of Interrupt
+ * On Change gpios connected to the radio.
+ *
+ * If this is a rising edge, the value of Timer5 is recorded. Otherwise,
+ * an entry is added in the buffer.
+ *
+ * @param[in] channel Index of the channel must be 0 or 1
+ */
 void RADIO_update(const uint8_t channel);
+
+/**
+ * @brief Check if a buffer is empty.
+ *
+ * @param[in] channel Index of the channel must be 0 or 1
+ */
 bool RADIO_is_buffer_empty(const uint8_t channel);
+
+/**
+ * @brief Retrieve one value from a buffer.
+ *
+ * Before calling this function, the buffer should always be checked if it is
+ * not empty. If that is the case, 0 is returned and a warning is printed.
+ *
+ * @param[in] channel Index of the channel must be 0 or 1
+ * @return Value in range 2000..4000, 0 if buffer is empty.
+ */
 uint16_t RADIO_buffer_read(const uint8_t channel);
 
 #ifdef	__cplusplus
